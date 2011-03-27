7b55277afc51a985cdd39b96056035a76698ac26
stefanooldeman <stefano.oldeman@gmail.com>
2011-03-27 15:19:28 +0200

    keep your secrets safe in a config mirror wich can be ignored by git

--- includes/connection.php
+++ includes/connection.php
@@ -1,10 +1,17 @@
-<?php // Connect	
-$connection = mysql_connect("localhost", "", "");
+<?php // Connect
+
+if(true) {
+	include 'config_mirror.php';
+} else {
+	include 'config.php';
+}
+
+$connection = mysql_connect("localhost", $mysql['db_user'], $mysql['db_password']);
 if (!$connection){
 	die("Database connection failed: ". mysql_error());
 }
 // DB Select
-$db_select = mysql_select_db("", $connection);
+$db_select = mysql_select_db($mysql['db_name'], $connection);
 if (!$db_select){
 	die("Database selection failed: ". mysql_error());
 } ?>
