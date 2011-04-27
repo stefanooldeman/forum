dbb350700e970a90d5c445eb4ca53fa8d6078482
stefanooldeman <stefano.oldeman@gmail.com>
2011-04-27 14:48:06 +0200

    hello jquery bye prototype

--- app/layout/header.php
+++ app/layout/header.php
@@ -4,7 +4,8 @@ $output .= '
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>audicious.com | w.i.p.</title>
-<script src="' . MEDIA_URL . 'js/prototype.js" type="text/javascript"></script>
+<script src="' . MEDIA_URL . 'js/jquery-1.5.2.min.js" type="text/javascript"></script>
+<script src="' . MEDIA_URL . 'js/base.js" type="text/javascript"></script>
 <link	href="' . MEDIA_URL . 'css/global.css" media="screen" type="text/css" rel="stylesheet" />
 </head>
 <body>';
